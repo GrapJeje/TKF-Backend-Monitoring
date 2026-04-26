dca824a0e42d4d930126650304949954d47d30b5
Jason <jasonvanl2019@gmail.com>
2026-04-26 09:37:00 +0200

    Added docker and an API server using ASP.NET Core

--- Program.cs
+++ Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using TKF_Backend_Monitoring.api;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+// API setup
+new ApiServer(app);
+
+app.Run("http://0.0.0.0:8080");
---
+++ api/ApiServer.cs
@@ -0,0 +1,44 @@
+﻿namespace TKF_Backend_Monitoring.api;
+
+public class ApiServer
+{
+    private readonly WebApplication app;
+    private readonly string authCode;
+
+    public ApiServer(WebApplication app)
+    {
+        this.app = app;
+        this.authCode = Environment.GetEnvironmentVariable("AUTH_CODE") ?? "test";
+
+        RegisterMiddleware();
+        RegisterRequests();
+    }
+
+    private void RegisterMiddleware()
+    {
+        app.Use(async (ctx, next) =>
+        {
+            var path = ctx.Request.Path;
+
+            if (path.StartsWithSegments("/health"))
+            {
+                await next();
+                return;
+            }
+
+            if (ctx.Request.Query["code"] != authCode)
+            {
+                ctx.Response.StatusCode = 401;
+                await ctx.Response.WriteAsync("Unauthorized");
+                return;
+            }
+
+            await next();
+        });
+    }
+
+    private void RegisterRequests()
+    {
+        app.MapGet("/health", () => Results.Ok("Server is running"));
+    }
+}
